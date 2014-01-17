557797fc7f372bfa0404e7c17418fb63c0cbddd9
Michel <orphee@gmail.com>
2014-01-17 01:16:37 -0800

    Rarity

--- run.js
+++ run.js
@@ -29,6 +29,7 @@ var run = Promise.coroutine(function* () {
         assert.equal(result[0], 0);
         assert.equal(result[1], 1);
         assert.equal(result[2], 2);
+        console.log(result);
 
 
         var result = yield r.expr({a: 0, b: 1}).run(connection);
@@ -44,7 +45,7 @@ var run = Promise.coroutine(function* () {
     }
 
 
-    console.log("Testing methods");
+    console.log("Testing add/sub/mul/div");
     try{
         var result = yield r.expr(1).add(1).run(connection);
         assert.equal(result, 2);
@@ -58,17 +59,69 @@ var run = Promise.coroutine(function* () {
         assert.equal(result, 3);
         console.log(result);
 
+        var result = yield r.expr(1).sub(1).run(connection);
+        assert.equal(result, 0);
+        console.log(result);
 
+        var result = yield r.expr(1).sub(1, 1).run(connection);
+        assert.equal(result, -1);
+        console.log(result);
+
+        var result = yield r.expr(2).mul(3).run(connection);
+        assert.equal(result, 6);
+        console.log(result);
+
+        var result = yield r.expr(2).mul(3, 4).run(connection);
+        assert.equal(result, 24);
+        console.log(result);
+
+        var result = yield r.expr(24).div(2).run(connection);
+        assert.equal(result, 12);
+        console.log(result);
+
+        var result = yield r.expr(24).div(2, 3).run(connection);
+        assert.equal(result, 4);
+        console.log(result);
     }
     catch(e) {
         console.log(e);
         throw e;
     }
 
-    /*
-    yield connection.close();
-    console.log("Connection closed");
-    */
+    console.log("Trying sequence");
+    try{
+        var result = yield r.db("test").info().run(connection);
+        console.log(result);
+
+        var result = yield r.db("test").table("test").info().run(connection);
+        console.log(result);
+
+        /*
+        var result = yield r.db("test").table("test").run(connection);
+        console.log(result);
+        */
+
+
+
+    }
+    catch(e) {
+        console.log('Error');
+        console.log(e);
+        throw e;
+    }
+
+
+
+    console.log("Closing connection");
+    try{
+        //var confirmation = yield connection.close();
+        var confirmation = yield connection.close({noReplyWait: true});
+        console.log("Connection closed");
+    }
+    catch(e) {
+        console.log(e);
+        throw e;
+    }
 })
 
 run();
