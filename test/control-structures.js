f8733aefb02338ec5bcf851daedfa72c308287c6
Michel <orphee@gmail.com>
2014-01-21 12:58:04 -0800

    Add control structures tests

--- test/control-structures.js
+++ test/control-structures.js
@@ -25,10 +25,98 @@ It("Init for `document-manipulation.js`", function* (done) {
     }
 })
 
-It("`r.branch` should work", function* (done) {
+It("`do` should work", function* (done) {
     try {
-        var result = yield r.branch(true, 1, 2).run(connection);
+        result = yield r.expr({a: 1}).do( function(doc) { return doc("a") }).run(connection);
         assert.equal(result, 1);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`branch` should work", function* (done) {
+    try {
+        result = yield r.branch(true, 1, 2).run(connection);
+        assert.equal(result, 1);
+
+        result = yield r.branch(false, 1, 2).run(connection);
+        assert.equal(result, 2);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`default` should work", function* (done) {
+    try {
+        result = yield r.expr({a:1})("b").default("Hello").run(connection);
+        assert.equal(result, "Hello");
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`r.js` should work", function* (done) {
+    try {
+        result = yield r.js("1").run(connection);
+        assert.equal(result, 1);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`coerceTo` should work", function* (done) {
+    try {
+        result = yield r.expr(1).coerceTo("STRING").run(connection);
+        assert.equal(result, "1");
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`typeOf` should work", function* (done) {
+    try {
+        result = yield r.expr(1).typeOf().run(connection);
+        assert.equal(result, "NUMBER");
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`json` should work", function* (done) {
+    try {
+        result = yield r.json('{"a":1}').run(connection);
+        assert.deepEqual(result, {a:1});
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`exprJSON` should work", function* (done) {
+    try {
+        result = yield r.exprJSON('{"a":1}').run(connection);
+        assert.deepEqual(result, {a:1});
+
         done();
     }
     catch(e) {
