5afd98eee1fb1fd1781267d7e41b42f5d48a9efb
Michel <orphee@gmail.com>
2014-01-21 10:44:33 -0800

    Add test for selecting data

--- test/selecting-data.js
+++ test/selecting-data.js
@@ -7,6 +7,7 @@ var assert = require('assert');
 var uuid = util.uuid;
 var connection; // global connection
 var dbName;
+var pks;
 
 function It(testName, generatorFn) {
     it(testName, function(done) {
@@ -27,6 +28,10 @@ It("Init for `selecting-data.js`", function* (done) {
 
         var result = yield r.db(dbName).tableCreate(tableName).run(connection);
         assert.deepEqual(result, {created:1});
+
+        result = yield r.db(dbName).table(tableName).insert(eval('['+new Array(100).join('{}, ')+'{}]')).run(connection);
+        assert.equal(result.inserted, 100);
+        pks = result.generated_keys;
 
         done();
     }
@@ -61,6 +66,172 @@ It("`table` should work", function* (done) {
         done(e);
     }
 })
+It("`get` should work", function* (done) {
+    try {
+        var result = yield r.db(dbName).table(tableName).get(pks[0]).run(connection);
+        assert.deepEqual(result, {id: pks[0]})
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+It("`getAll` should work with multiple values - primary key", function* (done) {
+    try {
+        var table = r.db(dbName).table(tableName);
+        query = table.getAll.apply(table, pks);
+        result = yield query.run(connection);
+        result = yield result.toArray();
+        assert.equal(result.length, 100);
+
+        table = r.db(dbName).table(tableName);
+        query = table.getAll.apply(table, pks.slice(0, 50));
+        result = yield query.run(connection);
+        result = yield result.toArray();
+        assert.equal(result.length, 50);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+It("`getAll` should work with multiple values - secondary index 1", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).update({field: 0}).run(connection);
+        assert.equal(result.replaced, 100);
+        result = yield r.db(dbName).table(tableName).sample(20).update({field: 10}).run(connection);
+        assert.equal(result.replaced, 20);
+
+        result = yield r.db(dbName).table(tableName).indexCreate("field").run(connection);
+        assert.deepEqual(result, {created: 1});
+
+        result = yield r.db(dbName).table(tableName).indexWait("field").run(connection);
+        result = yield result.toArray();
+        assert.deepEqual(result, [{"index":"field","ready":true}]);
+
+        result = yield r.db(dbName).table(tableName).getAll(10, {index: "field"}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 20);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+It("`getAll` should work with multiple values - secondary index 2", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).indexCreate("fieldAddOne", function(doc) { return doc("field").add(1) }).run(connection);
+        assert.deepEqual(result, {created: 1});
+
+        result = yield r.db(dbName).table(tableName).indexWait("fieldAddOne").run(connection);
+        result = yield result.toArray();
+        assert.deepEqual(result, [{"index":"fieldAddOne","ready":true}]);
+
+        result = yield r.db(dbName).table(tableName).getAll(11, {index: "fieldAddOne"}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 20);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`between` should wrok -- secondary index", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).between(5, 20, {index: "fieldAddOne"}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 20);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`filter` should work -- with an object", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).filter({field: 10}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 20);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+It("`filter` should work -- with an object -- looking for an undefined field", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).filter({nonExistingField: 10}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 0);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+
+It("`filter` should work -- with an anonymous function", function* (done) {
+    try {
+        cursor = yield r.db(dbName).table(tableName).filter(function(doc) { return doc("field").eq(10) }).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 20);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`filter` should work -- default true", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).filter({nonExistingField: 10}, {default: true}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 100);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+It("`filter` should work -- default false", function* (done) {
+    try {
+        result = yield r.db(dbName).table(tableName).filter({nonExistingField: 10}, {default: false}).run(connection);
+        assert(result);
+        result = yield result.toArray();
+        assert.equal(result.length, 0);
+
+        done();
+    }
+    catch(e) {
+        done(e);
+    }
+})
+
+
+
+
+
+
 
 
 It("End for `selecting-data.js`", function* (done) {
