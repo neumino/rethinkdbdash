53412db2d500d17a625254ab87056d87749a9f7d
Michel <orphee@gmail.com>
2014-01-31 01:10:06 -0800

    Fix tool to generate tests for backtraces

--- tool/generateTest.js
+++ tool/generateTest.js
@@ -8,20 +8,29 @@ var assert = require('assert');
 var dbName = util.uuid()
 var tableName = util.uuid()
 
-var query = 'r.expr(1).expr(2).add("foo")';
+var query;
+query = 'r.table("test").map( function(doc) { return r.expr(1).add("eh")})'
 
 Promise.coroutine(function* () {
     try {
+        yield r.dbCreate(dbName).run();
+        yield r.db(dbName).tableCreate(tableName).run();
+
         var result = yield eval(query).run();
+        throw new Error("Should have thrown an error");
     }
     catch(e) {
-        console.log("Query:")
-        console.log(query);
-        console.log('');
-        console.log("Backtrace:")
         console.log(e.message);
-        console.log('');
         console.log('')
+        console.log('')
+        console.log('')
+        console.log('/*');
+        console.log('Frames:');
+        console.log(e.frames);
+        console.log('');
+        console.log('Error:');
+        console.log(e.message.trim());
+        console.log('*/');
         console.log('It(\'Test backtrace for '+query+'\', function* (done) {')
         console.log('    try {');
         console.log('        r.nextVarId=1;');
@@ -30,7 +39,7 @@ Promise.coroutine(function* () {
         console.log('    }');
         console.log('    catch(e) {');
         //console.log('        if (e.message.replace(/var_[0-9]+/, "VAR") === '+JSON.stringify(e.message.replace(/var_[0-9]+/, "VAR")).replace('\\"'+dbName+'\\"', '\\""+dbName+"\\"').replace('\\"'+tableName+'\\"', '"+tableName+"')+') {');
-        console.log('        if (e.message === '+JSON.stringify(e.message).replace('\\"'+dbName+'\\"', '\\""+dbName+"\\"').replace('\\"'+tableName+'\\"', '"+tableName+"')+') {');
+        console.log('        if (e.message === '+JSON.stringify(e.message).replace('\\"'+dbName+'\\"', '\\""+dbName+"\\"').replace('\\"'+tableName+'\\"', '\\""+tableName+"\\"')+') {');
         console.log('            done()');
         console.log('        }');
         console.log('        else {');
