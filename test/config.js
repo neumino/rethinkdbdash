5fe1d5244fd9367e2ecbdd70a5040f797ee3090d
Michel <orphee@gmail.com>
2015-03-09 01:54:27 -0700

    Add parseInt for Wercker ports

--- test/config.js
+++ test/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     host: process.env['WERCKER_RETHINKDB_HOST'] || 'localhost',
-    port: process.env['WERCKER_RETHINKDB_PORT'] || 28015,
+    port: parseInt(process.env['WERCKER_RETHINKDB_PORT']) || 28015,
     authKey: '',
     buffer: 2,
     max: 5,
     fake_server: {
         host: process.env['WERCKER_RETHINKDB_HOST'] || 'localhost',
-        port: process.env['WERCKER_RETHINKDB_PORT']+1 || 28016,
+        port: parseInt(process.env['WERCKER_RETHINKDB_PORT'])+1 || 28016,
     },
     auto: false
 }
