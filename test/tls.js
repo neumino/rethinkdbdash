007b3d62ddd6915465caec43d51a42e39ab6b909
Michel <orphee@gmail.com>
2016-01-27 21:46:06 -0800

    Add test for tls with

--- test/tls.js
+++ test/tls.js
@@ -70,4 +70,35 @@ if (clientConfig.host === 'localhost') {
     }
     server.close();
   });
+
+
+  It("hello world for `tls.js` with TLS options and servers", function *(done) {
+    // Create the TLS server that will pipe to RethinkDB
+    var server = tls.createServer(tlsOpts, function (socket) {
+      var conn = net.createConnection(config.port, config.host);
+      socket.pipe(conn).pipe(socket);
+    }).listen(clientConfig.port);
+    //server.on('clientError', console.log);
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1"; // do not ignore self-signed certificates (default)
+    clientConfig.ssl = tlsOpts;
+    clientConfig.servers = [{
+      host: clientConfig.host,
+      port: clientConfig.port
+    }];
+    delete clientConfig.host;
+    delete clientConfig.port;
+
+    var r = require('../lib')(clientConfig);
+
+    try {
+      var result = yield r.expr(3).add(4).run()
+      assert.equal(7, result);
+      done();
+    }
+    catch(e) {
+      done(e);
+    }
+    server.close();
+  });
 }
